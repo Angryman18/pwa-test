3af1931dc65bde79b08d5f961f24369c32a2be7a
Angryman18 <imsmahanta@gmail.com>
2022-05-13 10:20:47 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "@material-tailwind/react/tailwind.css";
-import Button from "@material-tailwind/react/Button";
 
 function App() {
-  return <div className='text-lg'>Hello World This is my App</div>;
+  return <div className='text-lg'>Hello World This is my App
+  
+  </div>;
 }
 
 export default App;
